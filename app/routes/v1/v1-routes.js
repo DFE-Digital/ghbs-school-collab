666c2038c75a1d674ac7bd14fed96803e5f4f6db
Rich-Cooley <91479782+Rich-Cooley@users.noreply.github.com>
2024-12-13 13:19:53 +0000

    Add working tags to task list
    - logic it routes looks for complete, not started
      and anything else is in progress

--- app/routes/v1/v1-routes.js
+++ app/routes/v1/v1-routes.js
@@ -7,6 +7,18 @@ module.exports = function (router) {
   })
 
   router.post('/' + version + '/approach-to-market/complete-information-gathering', function (req, res) {
+    const tagCompleteInformationGathering = req.session.data['tagCompleteInformationGathering']
+
+    if (tagCompleteInformationGathering.includes('Review information gathered during triage') &
+      tagCompleteInformationGathering.includes('Send and review quality and scoring preferences document') &
+      tagCompleteInformationGathering.includes('empty')){
+      req.session.data.tagCompleteInformationGatheringStatus = 'complete'
+    }else if (tagCompleteInformationGathering == ('empty')){
+      req.session.data.tagCompleteInformationGatheringStatus = 'toDo'
+    }else {
+      req.session.data.tagCompleteInformationGatheringStatus = 'inProgress'
+    }
+
     res.redirect('/' + version + '/procurement')
   })
 }
