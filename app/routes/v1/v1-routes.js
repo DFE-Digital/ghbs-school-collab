5470142fae21d4a6c68cdaeb20ec4830743549a4
Rich-Cooley <91479782+Rich-Cooley@users.noreply.github.com>
2024-12-16 10:36:14 +0000

    File upload goes to new page
    File upload goes to common file upload page. When
    new files are uploaded increment the file version
    number.

--- app/routes/v1/v1-routes.js
+++ app/routes/v1/v1-routes.js
@@ -8,6 +8,7 @@ module.exports = function (router) {
 
   router.post('/' + version + '/approach-to-market/complete-information-gathering', function (req, res) {
     const tagCompleteInformationGathering = req.session.data['tagCompleteInformationGathering']
+    const pageAction = req.session.data['pageAction']
 
     if (tagCompleteInformationGathering.includes('Review information gathered during triage') &
       tagCompleteInformationGathering.includes('Send and review quality and scoring preferences document') &
@@ -19,6 +20,24 @@ module.exports = function (router) {
       req.session.data.tagCompleteInformationGatheringStatus = 'inProgress'
     }
 
-    res.redirect('/' + version + '/procurement')
+    if (pageAction == 'uploadInformationGathering'){
+      res.redirect('/' + version + '/upload-document')
+    } else {
+      res.redirect('/' + version + '/procurement')
+    }
+  })
+
+  router.get('/' + version + '/upload-document', function (req, res) {
+    res.render(version + '/upload-document', {})
+  })
+
+  router.post('/' + version + '/upload-document', function (req, res) {
+    const pageAction = req.session.data['pageAction']
+
+    if (pageAction == 'uploadInformationGathering'){
+      res.redirect('/' + version + '/approach-to-market/complete-information-gathering')
+    } else {
+      res.redirect('/' + version + '/procurement')
+    }
   })
 }
