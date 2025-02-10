37a05c27e988b2c22078570401cccf53df9dc724
Rich-Cooley <91479782+Rich-Cooley@users.noreply.github.com>
2025-02-10 15:49:18 +0000

    Enable testing of resolving overdue tasks
    This is a huge commit, that looks to set up the
    prototype as if some tasks are complete and two
    are overdue. This should let us show a procurement
    operations person how they might resolve this.
    
    There is a common change date page that allows
    users to set a new due date and resolve the
    overdue status

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -2,5 +2,13 @@ module.exports = {
 
   // Insert values here
   'procurementEstimatedValue': '£782,400',
-  'stage1ApprovalBlocked': 'true'
+  'stage1ApprovalBlocked': 'true',
+
+  // Task date offsets
+  'createProcurementTimeline' : '2',
+  'inviteTheSchoolLead' : '6',
+
+  // Tasks with errors
+  'researchFrameworksError': 'true',
+  'recordRouteToMarketError': 'true'
 }
