568d4c53c0d3e906acade42404154767c53dbcd5
Rich-Cooley <91479782+Rich-Cooley@users.noreply.github.com>
2025-02-28 14:05:13 +0000

    WIP trying to replicate cascading dates
    - add new timeline page page
    - add session data defaults for timeline
    - timeline page has a table of tasks with start
      and end dates and durations
    - need to figure out how to handle the cascading
      dates
    - fix missing /div

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -4,19 +4,43 @@ module.exports = {
   'procurementEstimatedValue': '£782,400',
   'stage1ApprovalBlocked': 'true',
 
-  // Task date offsets (cumulative)
+  // Task durations
   'handoverFromTriageDuration' : '1',
-  'discussSchoolRequirementsDuration' : '2',  // 1 + 1
-  'researchFrameworksDuration' : '7',         // 2 + 5
-  'recordRouteToMarketDuration' : '8',        // 7 + 1
-  'createProcurementTimeline' : '10',         // 8 + 2
-  'inviteTheSchoolLead' : '16',              // 10 + 6
-  'setProcurementTimelineDuration' : '17',    // 16 + 1
-  'createProcurementRiskAssessmentDuration' : '18', // 17 + 1
-  'checkCostThresholdDuration' : '19', // 18 + 1
-  'getApprovalDuration' : '30', // 19 + 11
-  'approvalFromSchoolOnApproachDuration' : '45', // 30 + 15
-  'stage1Duration' : '45', // 45 + 0
+  'discussSchoolRequirementsDuration' : '2',
+  'researchFrameworksDuration' : '5',
+  'recordRouteToMarketDuration' : '1',
+  'createProcurementTimelineDuration' : '1',
+  'inviteTheSchoolLeadDuration' : '1',
+  'setProcurementTimelineDuration' : '1',
+  'createProcurementRiskAssessmentDuration' : '1',
+  'checkCostThresholdDuration' : '1',
+  'getApprovalDuration' : '11',
+  'approvalFromSchoolOnApproachDuration' : '15',
+  'stage1Duration' : '45',
+
+  // Task start and end dates
+  'handoverFromTriageStartDate' : '2025-02-22',
+  'handoverFromTriageEndDate' : '2025-02-23',
+  'discussSchoolRequirementsStartDate' : '2025-02-23',
+  'discussSchoolRequirementsEndDate' : '2025-02-24',
+  'researchFrameworksStartDate' : '2025-02-24',
+  'researchFrameworksEndDate' : '2025-03-02',
+  'recordRouteToMarketStartDate' : '2025-03-02',
+  'recordRouteToMarketEndDate' : '2025-03-03',
+  'createProcurementTimelineStartDate' : '2025-03-03',
+  'createProcurementTimelineEndDate' : '2025-03-04',
+  'inviteTheSchoolLeadStartDate' : '2025-03-04',
+  'inviteTheSchoolLeadEndDate' : '2025-03-05',
+  'setProcurementTimelineStartDate' : '2025-03-05',
+  'setProcurementTimelineEndDate' : '2025-03-06',
+  'createProcurementRiskAssessmentStartDate' : '2025-03-06',
+  'createProcurementRiskAssessmentEndDate' : '2025-03-07',
+  'checkCostThresholdStartDate' : '2025-03-07',
+  'checkCostThresholdEndDate' : '2025-03-08',
+  'getApprovalStartDate' : '2025-03-08',
+  'getApprovalEndDate' : '2025-03-09',
+  'approvalFromSchoolOnApproachStartDate' : '2025-03-09',
+  'approvalFromSchoolOnApproachEndDate' : '2025-03-20',
 
   // Tasks with errors
   'tagResearchFrameworksStatus': 'overdue',
